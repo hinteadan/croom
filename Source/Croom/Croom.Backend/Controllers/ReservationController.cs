f030ee7ae0c2c3541cd1bfaa55aec85a0d36c4c7
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2013-08-22 16:43:41 +0300

    OD: Reservation interaction

--- Source/Croom/Croom.Backend/Controllers/ReservationController.cs
+++ Source/Croom/Croom.Backend/Controllers/ReservationController.cs
@@ -4,11 +4,29 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Croom.Backend.Infrastructure;
+using Croom.Data;
+using Croom.Data.Providers;
+using Croom.Engine;
+using Croom.Model;
+using Recognos.Core;
 
 namespace Croom.Backend.Controllers
 {
-    public class ReservationController : ApiController
+    public class ReservationController : BaseController
     {
+        private readonly ReservationEngine reservationEngine;
 
+        public ReservationController(IStoreDataAsKeyValue store)
+        {
+            Check.NotNull(store, "store");
+            reservationEngine = new ReservationEngine(CurrentUser, new ReservationProvider(store));
+            Check.InjectedMembers(this);
+        }
+
+        public void Post(Reservation reservation)
+        {
+            reservationEngine.AddReservation(reservation);
+        }
     }
 }
---
+++ Source/Croom/Croom.Backend/Infrastructure/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Croom.Model;
+
+namespace Croom.Backend.Infrastructure
+{
+    public abstract class BaseController : ApiController
+    {
+        private readonly User currentUser = new User("dummy", "Dummy User", "aaa@aaa.com");
+
+        protected User CurrentUser
+        {
+            get
+            {
+                return currentUser; 
+            }
+        }
+    }
+}

9d4fecd7d7121bcb83e71cf5772b69636b85920a
vlad.latis <vladimir.latis@recognos.ro>
2013-09-05 14:12:02 +0300

    main page development

--- Source/Croom/Croom.Frontend/Scripts/Controllers/home.js
+++ Source/Croom/Croom.Frontend/Scripts/Controllers/home.js
@@ -6,7 +6,9 @@
 
         scheduler.config.details_on_create = true;
         scheduler.config.details_on_dblclick = true;
+        scheduler.config.collision_limit = 3;
         scheduler.config.quick_info_detached = true;
+        scheduler.config.multi_day = false;
         scheduler.config.xml_date = "%Y-%m-%d %H:%i";
       
         scheduler.init('scheduler_here', new Date(), "week");
@@ -14,21 +16,47 @@
         var reservations = reservationApi.All(function () {
             _.each(reservations, function (item) {
                 item.Value.ServerId = item.Key;
-                item.Value.start_date = new Date(item.Value.StartsAt);
-                item.Value.end_date = new Date(item.Value.EndsAt);
+                item.Value.start_date = item.Value.StartsAt;
+                item.Value.end_date = item.Value.EndsAt;
                 item.Value.title = item.Value.Title;
             });
             var values = _.map(reservations, function (item) {
                 return item.Value;
             })
             scheduler.parse(values, "json");
+            scheduler.updateView(new Date(), "week");
         }),
             currentUser = userApi.Current();
 
-        //scheduler.parse([
-        //  { creator: "vladimir.latis", title: "first title", priority:"Normal", description: "asdasdasdsad\nasdsadsadad\n", start_date: "2013-08-30 09:00", end_date: "2013-08-30 12:00" },
-        //   { creator: "vladimir.latis", title: "second title", priority:"High", description: "asdasdasadsadsadsdsad\nasdsadsdsadadsadad\n", start_date: "2013-08-30 13:00", end_date: "2013-08-30 12:00" }
-        //], "json");
+        scheduler.attachEvent("onBeforeEventChanged", validateEventState);
+
+        function validateEventState(ev, e, flag, ev_old) {
+            var overlappingEvents = _.filter(scheduler.getEvents(), function (item) {
+                return item.id != ev.id && 
+                    ((item.start_date < ev.start_date && item.end_date > ev.end_date)
+                    ||(item.start_date > ev.start_date && item.end_date < ev.end_date)
+                    ||(item.start_date > ev.start_date && item.start_date < ev.end_date)
+                    ||(item.end_date > ev.start_date && item.end_date < ev.end_date));
+            });
+
+            if (overlappingEvents.length > 2) {
+                alert("Only 3 or fewer events can be scheduled simultaneous.");
+                scheduler.setEvent(ev.id, ev_old);
+                return false;
+            }
+
+            if (!scheduler.isOneDayEvent(ev)) {
+                alert("Multi days schedule is not allowed.");
+                scheduler.setEvent(ev.id, ev_old);
+                return false;
+            }
+            if (ev.start_date > ev.end_date) {
+                alert("End date must be greater than start date");
+                scheduler.setEvent(ev.id, ev_old);
+                return false;
+            }
+            return true;
+        };
 
         scheduler.showLightbox = function (id) {
             //if (!currentUser.IsUserLoggedIn){
@@ -42,9 +70,9 @@
             $scope.currentReservation.creator = ev.RequestedBy && ev.RequestedBy.FullNames || "";
             $scope.currentReservation.title = ev.title || "";
             $scope.currentReservation.description = ev.description || "";
-            $scope.currentReservation.priority = ev.Priority != undefined ? _.find($scope.priorities, { value: ev.Priority }) : $scope.priorities[1];
-            $scope.currentReservation.startDate = ev.start_date || "";
-            $scope.currentReservation.endDate = ev.end_date || "";
+            $scope.currentReservation.Priority = ev.Priority != undefined ? _.find($scope.priorities, { value: ev.Priority.value }) : $scope.priorities[1];
+            $scope.currentReservation.startDate = new Date(ev.start_date) || "";
+            $scope.currentReservation.endDate = new Date(ev.end_date) || "";
             
             if (!$scope.$$phase) {
                 $scope.$apply();
@@ -52,17 +80,33 @@
             scheduler.hideQuickInfo();
         };
 
+        function updateInternalSchedulerFields(event) {
+            event.text = event.title;
+            event.start_date = event.startDate;
+            event.end_date = event.endDate;
+        }
+
         $scope.saveEvent = function () {
-            var ev = scheduler.getEvent(scheduler.getState().lightbox_id);
+
+            var ev = scheduler.getEvent(scheduler.getState().lightbox_id),
+                eventClone = {};
+            $.extend(true, eventClone, ev);
+
             ev.creator = $scope.currentReservation.creator;
             ev.title = $scope.currentReservation.title;
             ev.description = $scope.currentReservation.description;
-            ev.priority = $scope.currentReservation.priority;
+            ev.Priority = $scope.currentReservation.Priority;
             ev.startDate = $scope.currentReservation.startDate;
             ev.endDate = $scope.currentReservation.endDate;
 
-            scheduler.endLightbox(true, $element.find('.lightbox-form')[0])
+            updateInternalSchedulerFields(ev);
+
+            if (validateEventState(ev, null, null, eventClone)) {
+                scheduler.endLightbox(true, $element.find('.lightbox-form')[0])
+            }
         }
+
+        $scope.isLoading = true;
 
         $scope.deleteEvent = function () {
             var event_id = scheduler.getState().lightbox_id;

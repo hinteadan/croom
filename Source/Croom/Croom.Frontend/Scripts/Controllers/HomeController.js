b706c54e1543380bea1a646c0702f4574ecada1a
Hintea Dan Alexandru <hinteadan@gmail.com>
2013-09-13 00:30:35 +0300

    Scaffold home/calendar page

---
+++ Source/Croom/Croom.Frontend/Scripts/Controllers/HomeController.js
@@ -0,0 +1,9 @@
+﻿(function (undefined) {
+    'use strict';
+
+    function HomeController($scope) {
+    }
+
+    this.controller('HomeController', ['$scope', HomeController]);
+
+}).call(this.Croom.AppModule);
--- Source/Croom/Croom.Frontend/Scripts/appConfig.js
+++ Source/Croom/Croom.Frontend/Scripts/appConfig.js
@@ -1,8 +1,19 @@
-﻿(function (undefined) {
+﻿(function (check, undefined) {
     'use strict';
+
+    function RouteInfo(controller, templateUrl) {
+        check.notEmpty(controller, 'controller');
+        check.notEmpty(templateUrl, 'templateUrl');
+
+        this.controller = controller;
+        this.templateUrl = templateUrl;
+    }
 
     this.config(['$routeProvider', 'AuthServiceProvider', function ($routeProvider, authProvider) {
         authProvider.ConfigureHeaders();
+        $routeProvider
+            .when('/', new RouteInfo('HomeController', 'Views/Calendar.html'))
+            .otherwise({ redirectTo: '/' });
     }]);
 
-}).call(this.Croom.AppModule);
+}).call(this.Croom.AppModule, this.Croom.Check);
